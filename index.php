14b156ee1e8b6db71954222fa2026f16493749d6
Michele Sputore <michele.sunnyvale@sunnyvale.it>
2019-01-22 15:51:02 +0100

    Change Title

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p style="color:blue;margin-left:30px;">Hello World-Happy Learning. This is wonderful! This is my first Jenkins PHP200 page using OpenShift Online</p>'; ?> 
+ <?php echo '<p style="color:blue;margin-left:30px;">Save My Quotes</p>'; ?> 
  </body>
 </html>
